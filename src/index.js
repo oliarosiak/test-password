4c3835bac3453432f27a69e54aaab7543001cb94
Olia Rosiak <olia.rosiak@gmail.com>
2023-02-03 16:45:33 +0200

    change 2

--- src/index.js
+++ src/index.js
@@ -1,29 +1,63 @@
 const inputField = document.querySelector('#user_password');
 const output = document.querySelector(".output");
 
-const blockOne = document.querySelector('.block_one');
-const blockTwo = document.querySelector('.block_two');
-const blockThree = document.querySelector('.block_three');
+const list = document.querySelector('.list');
+
+const firstBlock = list.firstElementChild;
+const secondBlock = firstBlock.nextElementSibling;
+const allThreeBlocks = document.querySelectorAll('li');
+
+if (inputField.value === 0) {
+    console.log(allThreeBlocks);
+    allThreeBlocks.forEach(element => {
+        element.classList.remove('short');
+        element.classList.remove('medium');
+        element.classList.remove('hard');  
+    })
+}
+
 
 inputField.addEventListener('input', passwordInput);
 
 function passwordInput() {
     const entered = inputField.value;
     const minLength = 8;
+    const mediumLength = 13;
+    const hardLength = 15;
+    const maxLength = 17;
 
     output.textContent = entered;
 
-    if (entered.length === 0) {
-        console.log('sho tut');
-        blockOne.classList.remove('short_password');
-        blockTwo.classList.remove('short_password');
-        blockThree.classList.remove('short_password');
-    }
+    // if (inputField.value === 0) {
+    //     allThreeBlocks.forEach(element => {
+    //         console.log(element);
+    //         element.classList.remove('short');
+    //         // element.classList.remove('medium');
+    //         // element.classList.remove('hard');  
+    //     })
+    // }
+
 
     if (entered.length> 0 && entered.length < minLength) {
-        console.log('закороткий');
-        blockOne.classList.add('short_password');
-        blockTwo.classList.add('short_password');
-        blockThree.classList.add('short_password');
-    }   
+        allThreeBlocks.forEach(element => {
+            element.classList.add('short');
+        })
+    } else if(entered.length > minLength) {
+        allThreeBlocks.forEach(element => {
+            element.classList.remove('short');
+        })
+        firstBlock.classList.add('short');
+    } else if(entered.length > mediumLength) {
+        allThreeBlocks.forEach(element => {
+            element.classList.remove('short');
+        });
+        firstBlock.classList.add('medium');
+        secondBlock.classList.add('medium');
+    } else if(entered.length > hardLength) {
+        allThreeBlocks.forEach(element => {
+            element.classList.add('hard');
+        });
+    }  
+
+  
 }
